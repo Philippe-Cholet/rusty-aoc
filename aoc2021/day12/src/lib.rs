a3b1d51bec5ec262f97d687633e039a3f21c596b
Philippe-Cholet <phcholet@orange.fr>
2023-01-08 18:11:23 +0100

    aoc21-12 four times faster
    Create owned strings and clone them was easier but slower.
    It is not the slowest solver by far but this fix was easy.

--- aoc2021/day12/src/lib.rs
+++ aoc2021/day12/src/lib.rs
@@ -2,36 +2,36 @@ use std::collections::{HashMap, HashSet};
 
 use common::{ensure, Context, Part, Part2, Result};
 
-type Graph = HashMap<String, HashSet<String>>;
+type Graph<'a> = HashMap<&'a str, HashSet<&'a str>>;
 
 fn is_big(name: &str) -> bool {
     name.chars().all(char::is_uppercase)
 }
 
 #[derive(Debug)]
-struct Path {
-    points: Vec<String>,
+struct Path<'a> {
+    points: Vec<&'a str>,
     bonus: bool,
 }
 
-impl Path {
+impl<'a> Path<'a> {
     fn unvisited(&self, name: &str) -> bool {
-        !self.points.contains(&name.to_owned())
+        !self.points.contains(&name)
     }
 
     #[allow(clippy::expect_used)]
-    fn neighbors(&self, graph: &Graph) -> Vec<String> {
+    fn neighbors(&self, graph: &'a Graph) -> Vec<&'a str> {
         let last = self.points.last().expect("points is never empty");
         graph[last]
             .iter()
             .filter(|next| self.bonus || is_big(next) || self.unvisited(next))
-            .map(String::clone)
+            .copied()
             .collect()
     }
 
-    fn push(&self, name: &str) -> Option<Self> {
+    fn push(&self, name: &'a str) -> Option<Self> {
         let mut points = self.points.clone();
-        points.push(name.to_owned());
+        points.push(name);
         if is_big(name) || self.unvisited(name) {
             Some(Self {
                 points,
@@ -53,30 +53,24 @@ pub fn solver(part: Part, input: &str) -> Result<String> {
     let mut graph: Graph = HashMap::new();
     for line in input.lines() {
         let (u, v) = line.split_once('-').context("No delimiter")?;
-        graph
-            .entry(u.to_owned())
-            .or_insert_with(HashSet::new)
-            .insert(v.to_owned());
-        graph
-            .entry(v.to_owned())
-            .or_insert_with(HashSet::new)
-            .insert(u.to_owned());
+        graph.entry(u).or_insert_with(HashSet::new).insert(v);
+        graph.entry(v).or_insert_with(HashSet::new).insert(u);
     }
     ensure!(
         graph.contains_key("start") && graph.contains_key("end"),
         "The graph does not contain an endpoint"
     );
     let mut stack = vec![Path {
-        points: vec!["start".to_owned()],
+        points: vec!["start"],
         bonus: part == Part2,
     }];
     let mut nb_paths = 0;
     while let Some(path) = stack.pop() {
         for neighbor in path.neighbors(&graph) {
-            if &neighbor == "end" {
+            if neighbor == "end" {
                 // println!("{},end (bonus={})", path.points.join(","), path.bonus);
                 nb_paths += 1;
-            } else if let Some(new_path) = path.push(&neighbor) {
+            } else if let Some(new_path) = path.push(neighbor) {
                 stack.push(new_path);
             }
         }
