09383a63b9478f1c427104b6d3703b99023c9b7e
Philippe-Cholet <phcholet@orange.fr>
2023-01-13 17:08:45 +0100

    aoc21-08: 22 times faster
    Use owned strings and hashs was slow. The approach is identical (still 5040 permutations to consider) but use `u8` to represent a seven segment display is definitely lighter.
    And use `u128` to represent an unordered collection of `u8` is a bit tricky (and probably not as essential to speed, sort vectors of 10 integers can not be slow) but I like it.
    I also added the simple example to tests.

--- aoc2021/day08/src/lib.rs
+++ aoc2021/day08/src/lib.rs
@@ -1,87 +1,142 @@
-use std::collections::{HashMap, HashSet};
-
 use itertools::Itertools;
 
-use common::{Context, Ok, Part, Part1, Part2, Result};
+use common::{ensure, Context, Error, Part, Part1, Part2, Result};
 use utils::OkIterator;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Segm7(u8);
+
+impl std::str::FromStr for Segm7 {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut n = 0;
+        for b in s.bytes() {
+            ensure!(matches!(b, b'a'..=b'g'), "Not in a-g range");
+            let flag = 1 << (b - b'a');
+            ensure!(n & flag == 0, "Duplicate segment");
+            n |= flag;
+        }
+        Ok(Self(n))
+    }
+}
+
+impl std::fmt::Display for Segm7 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, ch) in "abcdefg".chars().enumerate() {
+            if self.0 & (1 << i) != 0 {
+                write!(f, "{ch}")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Segm7 {
+    #[allow(clippy::unreadable_literal)]
+    const DIGITS: [Self; 10] = [
+        //     gfedcba
+        Self(0b1110111), // 0: abcefg
+        Self(0b0100100), // 1: cf
+        Self(0b1011101), // 2: acdeg
+        Self(0b1101101), // 3: acdfg
+        Self(0b0101110), // 4: bcdf
+        Self(0b1101011), // 5: abdfg
+        Self(0b1111011), // 6: abdefg
+        Self(0b0100101), // 7: acf
+        Self(0b1111111), // 8: abcdefg
+        Self(0b1101111), // 9: abcdfg
+    ];
+
+    const fn count_segments(self) -> u32 {
+        self.0.count_ones()
+    }
+
+    fn bitset(segments: &[Self]) -> u128 {
+        segments.iter().fold(0, |n, seg| n | (1 << seg.0))
+    }
+
+    fn permute(self, perm: &[u8]) -> Self {
+        Self(
+            perm.iter()
+                .enumerate()
+                .fold(0, |res, (i, p)| res | ((self.0 >> i) & 1) << p),
+        )
+    }
+
+    fn to_digit(self) -> Option<usize> {
+        Self::DIGITS
+            .into_iter()
+            .enumerate()
+            .find_map(|(digit, seg)| (seg == self).then_some(digit))
+    }
+
+    fn read_number(numbers: &[Self], perm: &[u8]) -> Result<usize> {
+        // In the example: abcdefg --> cfgabde
+        //  aaaa       dddd
+        // b    c     e    a
+        // b    c     e    a
+        //  dddd  -->  ffff
+        // e    f     g    b
+        // e    f     g    b
+        //  gggg       cccc
+        // println!("{}", (0..7).map(|i| Self(1 << i).permute(perm)).join(""));
+        numbers
+            .iter()
+            .map(|seg| {
+                seg.permute(perm)
+                    .to_digit()
+                    .context("Not a seven segment digit")
+            })
+            .ok_fold(0, |res, digit| res * 10 + digit)
+    }
+}
 
 /// Seven Segment Search
 pub fn solver(part: Part, input: &str) -> Result<String> {
-    let data: Vec<([&str; 10], [&str; 4])> = input
+    let data: Vec<([Segm7; 10], [Segm7; 4])> = input
         .lines()
         .map(|line| {
-            let (signals, four) = line.split_once(" | ").context("no delimiter")?;
-            let signals: Vec<_> = signals.split(' ').collect();
-            let four: Vec<_> = four.split(' ').collect();
-            Ok((
-                signals.try_into().ok().context("Not 10 elements")?,
-                four.try_into().ok().context("Not 4 elements")?,
+            let (signal, entry) = line.split_once(" | ").context("no delimiter")?;
+            common::Ok((
+                signal
+                    .split_whitespace()
+                    .map(str::parse)
+                    .ok_collect_array()?,
+                entry
+                    .split_whitespace()
+                    .map(str::parse)
+                    .ok_collect_array()?,
             ))
         })
         .ok_collect()?;
-    let result = match part {
+    Ok(match part {
         Part1 => data
-            .iter()
-            .flat_map(|(_signal, four)| {
-                four.iter()
-                    .filter(|digit| [2, 3, 4, 7].contains(&digit.len()))
-            })
+            .into_iter()
+            .flat_map(|line| line.1)
+            .filter(|seg| [2, 3, 4, 7].contains(&seg.count_segments()))
             .count(),
         Part2 => {
-            let segments = [
-                "abcefg".to_owned(),  // 0
-                "cf".to_owned(),      // 1
-                "acdeg".to_owned(),   // 2
-                "acdfg".to_owned(),   // 3
-                "bcdf".to_owned(),    // 4
-                "abdfg".to_owned(),   // 5
-                "abdefg".to_owned(),  // 6
-                "acf".to_owned(),     // 7
-                "abcdefg".to_owned(), // 8
-                "abcdfg".to_owned(),  // 9
-            ];
-            let segment2idx: HashMap<String, usize> = segments
-                .iter()
-                .enumerate()
-                .map(|(idx, s)| (s.clone(), idx))
-                .collect();
-            let answer = HashSet::from(segments);
-            let translate = |s: &str, table: &HashMap<char, char>| -> String {
-                let mut new_s: Vec<char> = s.chars().map(|c| table[&c]).collect();
-                new_s.sort_unstable();
-                new_s.into_iter().collect()
-            };
-            data.iter()
-                .map(|(signal, four)| {
-                    "abcdefg"
-                        .chars()
-                        .permutations(7)
+            let digit_bitset = Segm7::bitset(&Segm7::DIGITS);
+            data.into_iter()
+                .flat_map(|(signal, entry)| {
+                    (0..7)
+                        .permutations(7) // 5040 possibilities
                         .find_map(|perm| {
-                            let table: HashMap<char, char> = "abcdefg".chars().zip(perm).collect();
-                            if (signal
-                                .iter()
-                                .map(|s| translate(s, &table))
-                                .collect::<HashSet<_>>())
-                                == answer
-                            {
-                                Some(
-                                    four.map(|s| segment2idx[&translate(s, &table)])
-                                        .iter()
-                                        .fold(0, |prev, d| prev * 10 + d),
-                                )
-                            } else {
-                                None
-                            }
+                            let perm_signal = signal.map(|seg| seg.permute(&perm));
+                            (Segm7::bitset(&perm_signal) == digit_bitset)
+                                .then(|| Segm7::read_number(&entry, &perm))
                         })
                         .context("No solution")
                 })
                 .ok_sum()?
         }
-    };
-    Ok(result.to_string())
+    }
+    .to_string())
 }
 
-pub const INPUTS: [&str; 2] = [
+pub const INPUTS: [&str; 3] = [
+    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
     "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
@@ -98,9 +153,11 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
 #[test]
 fn solver_21_08() -> Result<()> {
-    assert_eq!(solver(Part1, INPUTS[0])?, "26");
-    assert_eq!(solver(Part1, INPUTS[1])?, "301");
-    assert_eq!(solver(Part2, INPUTS[0])?, "61229");
-    assert_eq!(solver(Part2, INPUTS[1])?, "908067");
+    assert_eq!(solver(Part1, INPUTS[0])?, "0");
+    assert_eq!(solver(Part1, INPUTS[1])?, "26");
+    assert_eq!(solver(Part1, INPUTS[2])?, "301");
+    assert_eq!(solver(Part2, INPUTS[0])?, "5353");
+    assert_eq!(solver(Part2, INPUTS[1])?, "61229");
+    assert_eq!(solver(Part2, INPUTS[2])?, "908067");
     Ok(())
 }
