df3388a6314ff99e8c862faa82ee5492257962d5
Philippe-Cholet <phcholet@orange.fr>
2023-02-04 16:43:56 +0100

    2020 day 1

---
+++ aoc2020/day01/src/lib.rs
@@ -0,0 +1,43 @@
+use itertools::Itertools;
+
+use common::{Context, Part, Part1, Part2, Result};
+use utils::OkIterator;
+
+const TOTAL: u32 = 2020;
+
+/// Report Repair
+pub fn solver(part: Part, input: &str) -> Result<String> {
+    let items = input.lines().map(str::parse::<u32>).ok_collect_hset()?;
+    Ok(match part {
+        Part1 => items.iter().find_map(|a| {
+            let b = TOTAL.checked_sub(*a)?;
+            items.contains(&b).then(|| a * b)
+        }),
+        Part2 => items.iter().tuple_combinations().find_map(|(a, b)| {
+            let c = TOTAL.checked_sub(a + b)?;
+            items.contains(&c).then(|| a * b * c)
+        }),
+    }
+    .context("No solution")?
+    .to_string())
+}
+
+pub const INPUTS: [&str; 2] = [
+    "1721
+979
+366
+299
+675
+1456
+",
+    include_str!("input.txt"),
+];
+
+#[test]
+fn solver_20_01() -> Result<()> {
+    assert_eq!(solver(Part1, INPUTS[0])?, "514579"); // 1721 * 299
+    assert_eq!(solver(Part1, INPUTS[1])?, "1016964");
+    assert_eq!(solver(Part2, INPUTS[0])?, "241861950"); // 979 * 366 * 675
+    assert_eq!(solver(Part2, INPUTS[1])?, "182588480");
+    Ok(())
+}
