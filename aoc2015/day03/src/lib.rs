3c22931d3a3ec4fd716e0fe045ca1b8abb26b031
Philippe-Cholet <phcholet@orange.fr>
2023-02-20 17:18:31 +0100

    2015 day 3

---
+++ aoc2015/day03/src/lib.rs
@@ -0,0 +1,56 @@
+use std::collections::HashSet;
+
+use common::{bail, Part, Part1, Part2, Result};
+use utils::OkIterator;
+
+/// Perfectly Spherical Houses in a Vacuum
+pub fn solver(part: Part, input: &str) -> Result<String> {
+    let data = input
+        .trim_end()
+        .chars()
+        .map(|ch| {
+            Ok(match ch {
+                '^' => (-1, 0),
+                'v' => (1, 0),
+                '>' => (0, 1),
+                '<' => (0, -1),
+                _ => bail!("Expected ^ v > or < but got {:?}.", ch),
+            })
+        })
+        .ok_collect_vec()?;
+    let mut houses: HashSet<_> = match part {
+        Part1 => {
+            let mut loc = (0, 0);
+            data.into_iter()
+                .map(|(dr, dc)| {
+                    loc.0 += dr;
+                    loc.1 += dc;
+                    loc
+                })
+                .collect()
+        }
+        Part2 => {
+            let mut locs = [(0, 0); 2];
+            data.into_iter()
+                .enumerate()
+                .map(|(idx, (dr, dc))| {
+                    let loc = &mut locs[idx % 2];
+                    loc.0 += dr;
+                    loc.1 += dc;
+                    *loc
+                })
+                .collect()
+        }
+    };
+    houses.insert((0, 0));
+    Ok(houses.len().to_string())
+}
+
+pub const INPUTS: [&str; 1] = [include_str!("input.txt")];
+
+#[test]
+fn solver_15_03() -> Result<()> {
+    assert_eq!(solver(Part1, INPUTS[0])?, "2572");
+    assert_eq!(solver(Part2, INPUTS[0])?, "2631");
+    Ok(())
+}
