dd21c81a81287f3cf139cb10577f074b40c8e605
Philippe-Cholet <phcholet@orange.fr>
2023-06-25 12:40:03 +0200

    aoc15-04: 44% faster
    Instead of converting 1 2 3 4... to string (in order), I manually manage the underlying `Vec<u8>`. Increment is way faster.
    Down to 0.68 second, instead of 0.98 second.

--- aoc2015/day04/src/lib.rs
+++ aoc2015/day04/src/lib.rs
@@ -8,15 +8,32 @@ pub fn solver(part: Part, input: &str) -> Result<String> {
     };
     let mut context = md5::Context::new();
     context.consume(input.trim_end());
+    let mut data = vec![b'0'];
     Ok((1..)
         .find_map(|n| {
             let mut ctx = context.clone();
-            ctx.consume(n.to_string());
+            increment(&mut data);
+            ctx.consume(&data);
             let digest = ctx.compute();
             (&digest.0[..3] <= &max3).then_some(n)
         })
         .context("No solution")?
         .to_string())
+}
+
+fn increment(data: &mut Vec<u8>) {
+    if let Some(idx) = data.iter().rposition(|x| x != &b'9') {
+        for n in data.iter_mut().skip(idx + 1) {
+            *n = b'0';
+        }
+        data[idx] += 1;
+    } else {
+        for n in data.iter_mut() {
+            *n = b'0';
+        }
+        data.push(b'0');
+        data[0] += 1;
+    }
 }
 
 pub const INPUTS: [&str; 2] = ["abcdef", include_str!("input.txt")];
@@ -27,4 +44,16 @@ fn solver_15_04() -> Result<()> {
     assert_eq!(solver(Part1, INPUTS[1])?, "117946");
     assert_eq!(solver(Part2, INPUTS[1])?, "3938038");
     Ok(())
+}
+
+#[test]
+#[ignore]
+fn test_increment() {
+    for n in 0..=100 {
+        let mut data = vec![b'0'];
+        for _ in 0..n {
+            increment(&mut data);
+        }
+        assert_eq!(data, n.to_string().into_bytes(), "{}", n);
+    }
 }
