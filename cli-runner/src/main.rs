9facc20a7f3cb8eafa10c674d385dcc7de482071
Philippe-Cholet <phcholet@orange.fr>
2023-07-21 23:06:01 +0200

    Reorder `cli-runner`
    Also add an help message and total timings per day in `run_big_inputs`.

--- cli-runner/src/main.rs
+++ cli-runner/src/main.rs
@@ -14,40 +14,20 @@ fn timed_solve(solver: AocSolver, part: Part, input: &str) -> Result<(String, Du
     Ok((result, t))
 }
 
-fn main() -> Result<()> {
-    let mut args = env::args().skip(1);
-    // Get the solver for a given puzzle (year, day).
-    let year = match args.next() {
-        Some(s) => s.parse()?,
-        None => return run_big_inputs(None),
-    };
-    let day = match args.next() {
-        Some(s) => s.parse()?,
-        None => return run_big_inputs(Some(year)),
-    };
+fn run_some_inputs(year: Year, day: Day, parts: &[Part], index: Option<usize>) -> Result<()> {
     let (solver, inputs) = aoc(year, day)?;
-    // Given part or both.
-    let parts = match args.next() {
-        Some(s) => vec![s.parse()?],
-        None => Part::ALL.to_vec(),
-    };
     // Given index or all.
-    let input_indexes = match args.next() {
-        Some(s) => {
-            let idx: usize = s.parse()?;
-            ensure!(
-                idx < inputs.len(),
-                "No input #{}, there are only {} inputs!",
-                idx,
-                inputs.len()
-            );
+    let input_indexes = match index {
+        Some(idx) => {
+            let nb = inputs.len();
+            ensure!(idx < nb, "No input #{idx}, there are only {nb} inputs!");
             vec![idx]
         }
         None => (0..inputs.len()).collect(),
     };
     // Run the solver on selected parts and inputs.
     println!("Advent of Code {year:?} {day:?}...");
-    for part in parts {
+    for &part in parts {
         for input_idx in &input_indexes {
             println!("\n{part:?} input #{input_idx}:");
             let input = inputs[*input_idx];
@@ -59,14 +39,8 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn run_big_inputs(year: Option<Year>) -> Result<()> {
-    let Some(year) = year else {
-        for year in Year::ALL {
-            run_big_inputs(Some(year))?;
-        }
-        return Ok(());
-    };
-    let mut results = vec![];
+fn run_big_inputs(year: Year) -> Result<()> {
+    let mut results = Vec::with_capacity(50);
     for day in Day::ALL {
         if let Ok((solver, [.., big_input])) = aoc(year, day) {
             let t1 = timed_solve(solver, Part1, big_input)?.1;
@@ -78,7 +52,7 @@ fn run_big_inputs(year: Option<Year>) -> Result<()> {
     if !results.is_empty() {
         println!("========== {year:?} ==========");
         for (day, t1, t2) in &results {
-            println!("{day:?}: {t1:?} + {t2:?}");
+            println!("{day:?}: {t1:?} + {t2:?} == {:?}", *t1 + *t2);
         }
         let t1s: Duration = results.iter().map(|(_, t1, _)| t1).sum();
         let t2s: Duration = results.iter().map(|(_, _, t2)| t2).sum();
@@ -89,4 +63,65 @@ fn run_big_inputs(year: Option<Year>) -> Result<()> {
         );
     }
     Ok(())
+}
+
+const HELP: &str = "\
+USAGE:
+  cargo run [YEAR [DAY [PART [INDEX]]]]
+
+FLAGS:
+  -h, --help         Prints help information
+
+ARGS:
+  YEAR    [20]15..   Or run all years only on your big inputs   timings only
+  DAY     1..=25     Or run all days only on your big inputs    timings only
+  PART    1 | 2      Or run all parts on all inputs             answers & timings
+  INDEX   0..        Or run all inputs                          answers & timings
+";
+
+#[derive(Debug)]
+struct Args {
+    year: Option<Year>,
+    day: Option<Day>,
+    part: Option<Part>,
+    index: Option<usize>,
+}
+
+impl Args {
+    fn from_env() -> Result<Self> {
+        let args: Vec<_> = env::args().skip(1).collect();
+        if args.iter().any(|s| ["-h", "--help"].contains(&s.as_str())) {
+            print!("{HELP}");
+            std::process::exit(0);
+        }
+        ensure!(args.len() <= 4, "Up to four arguments expected");
+        Ok(Self {
+            year: args.get(0).map(|s| s.parse()).transpose()?,
+            day: args.get(1).map(|s| s.parse()).transpose()?,
+            part: args.get(2).map(|s| s.parse()).transpose()?,
+            index: args.get(3).map(|s| s.parse()).transpose()?,
+        })
+    }
+
+    fn run(&self) -> Result<()> {
+        // All solvers on big inputs.
+        let Some(year) = self.year else {
+            for year in Year::ALL {
+                run_big_inputs(year)?;
+            }
+            return Ok(());
+        };
+        // All solvers of the given year on big inputs.
+        let Some(day) = self.day else {
+            return run_big_inputs(year);
+        };
+        // Given part or both.
+        let parts = self.part.map_or_else(|| Part::ALL.to_vec(), |p| vec![p]);
+        // Some part(s) and some inputs for a given puzzle.
+        run_some_inputs(year, day, &parts, self.index)
+    }
+}
+
+fn main() -> Result<()> {
+    Args::from_env()?.run()
 }
