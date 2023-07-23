02f8b484ae5186913857613fc859749c619a40c0
Philippe-Cholet <phcholet@orange.fr>
2023-07-23 11:32:20 +0200

    aoc21-19: 24 permutations, not 48
    It's the currently slowest solution of 2021 and I tried many times to fasten this without addressing the elephant in the room: the 48 rotations when 24 should be enough. I finally do!
    After some struggle, I came up with what I think is an elegant descriptive solution. Which axis is `Z` ? Is it up or down? And how rotated is it around it.
    Now all 2021 runs around 775ms, finally under 1s. 🤩

--- aoc2021/day19/src/lib.rs
+++ aoc2021/day19/src/lib.rs
@@ -7,25 +7,32 @@ use itertools::{iproduct, Itertools};
 
 use common::{prelude::*, Ok};
 
-#[derive(Debug, PartialEq, Hash, Eq)]
+#[derive(Debug, Clone, PartialEq, Hash, Eq)]
 struct Xyz(i32, i32, i32);
 
-impl Xyz {
-    fn arrange(&self, sx: bool, sy: bool, sz: bool, p: u8) -> Self {
-        let x = if sx { -self.0 } else { self.0 };
-        let y = if sy { -self.1 } else { self.1 };
-        let z = if sz { -self.2 } else { self.2 };
-        match p {
-            0 => Self(x, y, z),
-            1 => Self(y, x, z), // x <-> y
-            2 => Self(z, y, x), // x <-> z
-            3 => Self(x, z, y), // y <-> z
-            4 => Self(z, x, y), // x --> y --> z --> x
-            5 => Self(y, z, x), // x <-- y <-- z <-- x
-            _ => unreachable!("p is supposed to be in 0..6 but p={p}"),
-        }
-    }
+#[derive(Debug, Clone, Copy)]
+enum Axis3D {
+    X,
+    Y,
+    Z,
+}
 
+#[derive(Debug, Clone, Copy)]
+struct Up(bool);
+
+#[derive(Debug, Clone, Copy)]
+enum Rot {
+    Rot0,
+    Rot90,
+    Rot180,
+    Rot270,
+}
+
+trait Permute<T> {
+    fn permute(self, item: T) -> T;
+}
+
+impl Xyz {
     const fn norm(&self) -> u32 {
         self.0.unsigned_abs() + self.1.unsigned_abs() + self.2.unsigned_abs()
     }
@@ -47,12 +54,63 @@ impl Sub for &Xyz {
     }
 }
 
-// All 48 changes are considered here...
+impl Permute<Xyz> for Axis3D {
+    /// Which is the `Z` axis?
+    #[inline]
+    fn permute(self, Xyz(x, y, z): Xyz) -> Xyz {
+        match self {
+            Self::X => Xyz(y, z, x),
+            Self::Y => Xyz(z, x, y),
+            Self::Z => Xyz(x, y, z),
+        }
+    }
+}
+
+impl Permute<Xyz> for Up {
+    /// The `Z` axis is up or down?
+    #[inline]
+    fn permute(self, Xyz(x, y, z): Xyz) -> Xyz {
+        if self.0 {
+            Xyz(x, y, z)
+        } else {
+            Xyz(y, x, -z)
+        }
+    }
+}
+
+impl Permute<Xyz> for Rot {
+    /// Rotations around the `Z` axis.
+    #[inline]
+    fn permute(self, Xyz(x, y, z): Xyz) -> Xyz {
+        match self {
+            Self::Rot0 => Xyz(x, y, z),
+            Self::Rot90 => Xyz(-y, x, z),
+            Self::Rot180 => Xyz(-x, -y, z),
+            Self::Rot270 => Xyz(y, -x, z),
+        }
+    }
+}
+
+// Let's be fancy!
+impl<T, A: Permute<T>, B: Permute<T>, C: Permute<T>> Permute<T> for (A, B, C) {
+    /// Permute for each parameter.
+    #[inline]
+    fn permute(self, item: T) -> T {
+        self.2.permute(self.1.permute(self.0.permute(item)))
+    }
+}
+
 fn merge12(positions: &HashSet<Xyz>, group: &[Xyz]) -> Option<(Xyz, Vec<Xyz>)> {
-    iproduct!([false, true], [false, true], [false, true], 0..6).find_map(|(sx, sy, sz, p)| {
+    iproduct!(
+        [Axis3D::X, Axis3D::Y, Axis3D::Z],
+        [Up(true), (Up(false))],
+        [Rot::Rot0, Rot::Rot90, Rot::Rot180, Rot::Rot270]
+    )
+    .find_map(|transformation| {
         let aligned = group
             .iter()
-            .map(|xyz| xyz.arrange(sx, sy, sz, p))
+            .cloned()
+            .map(|xyz| transformation.permute(xyz))
             .collect_vec();
         let mut offsets = HashMap::<_, usize>::with_capacity(positions.len() * aligned.len());
         iproduct!(positions.iter(), aligned.iter())
