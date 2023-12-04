e9e17b5ac1a48c1bf87f76d204c2ca2537508ce4
Philippe-Cholet <phcholet@orange.fr>
2023-12-04 12:45:57 +0100

    aoc23-04

---
+++ aoc2023/day04/src/lib.rs
@@ -0,0 +1,62 @@
+use itertools::Itertools;
+
+use common::prelude::*;
+
+/// Scratchcards
+pub fn solver(part: Part, input: &str) -> Result<String> {
+    let data: Vec<u8> = input
+        .lines()
+        .enumerate()
+        .map(|(idx, line)| {
+            let (card, cards) = line.split_once(':').context("colon delimiter")?;
+            let (card, id) = card.split_whitespace().collect_tuple().context("Card #")?;
+            ensure!(card == "Card");
+            ensure!(id.parse::<usize>()? == idx + 1);
+            let (winning, my) = cards.split_once('|').context("| delimiter")?;
+            let winning = winning.split_whitespace().collect_vec();
+            Ok(my
+                .split_whitespace()
+                .filter(|nb| winning.contains(nb))
+                .count() // I do not have more than `u8::MAX` winning cards.
+                .try_into()?)
+        })
+        .try_collect()?;
+    Ok(match part {
+        Part1 => data
+            .iter()
+            .filter_map(|n| n.checked_sub(1).map(|times_doubled| 1u32 << times_doubled))
+            .sum::<u32>(),
+        Part2 => {
+            let mut counts = vec![1; data.len()];
+            for (idx, new_cards) in data.iter().enumerate().rev() {
+                counts[idx] += counts[idx + 1..][..*new_cards as usize]
+                    .iter()
+                    .copied()
+                    .sum::<u32>();
+            }
+            counts.into_iter().sum()
+        }
+    }
+    .to_string())
+}
+
+pub const INPUTS: [&str; 2] = [
+    "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+",
+    include_str!("input.txt"),
+];
+
+#[test]
+fn solver_23_04() -> Result<()> {
+    assert_eq!(solver(Part1, INPUTS[0])?, "13");
+    assert_eq!(solver(Part1, INPUTS[1])?, "17803");
+    assert_eq!(solver(Part2, INPUTS[0])?, "30");
+    assert_eq!(solver(Part2, INPUTS[1])?, "5554894");
+    Ok(())
+}
