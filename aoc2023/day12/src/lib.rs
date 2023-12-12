27556f6dfccc29157320715fcad24db58fa085f5
Philippe-Cholet <phcholet@orange.fr>
2023-12-12 22:16:41 +0100

    aoc23-12: 10 times faster (cache)

--- aoc2023/day12/src/lib.rs
+++ aoc2023/day12/src/lib.rs
@@ -6,6 +6,7 @@ use self::Record::{Damaged, Operational, Unknown};
 
 /// Hot Springs
 pub fn solver(part: Part, input: &str) -> Result<usize> {
+    let mut cache = HashMap::with_capacity(256);
     input
         .lines()
         .map(|line| {
@@ -26,8 +27,11 @@ pub fn solver(part: Part, input: &str) -> Result<usize> {
             Ok((records, nbs))
         })
         .process_results(|it| {
-            it.map(|(records, nums)| multiple_damaged_groups(&records, &nums))
-                .sum()
+            it.map(|(records, nums)| {
+                cache.clear();
+                multiple_damaged_groups(&mut cache, &records, &nums)
+            })
+            .sum()
         })
 }
 
@@ -85,44 +89,85 @@ fn one_damaged_group(records: &[Record], num: usize) -> usize {
 }
 
 // Some divide-and-conquer algorithm
-fn multiple_damaged_groups(records: &[Record], nums: &[u8]) -> usize {
+fn multiple_damaged_groups(
+    cache: &mut HashMap<CacheKey, usize>,
+    records: &[Record],
+    nums: &[u8],
+) -> usize {
+    let key = cache_key(records, nums);
+    if let Some(count) = cache.get(&key) {
+        return *count;
+    }
     let n = nums.len();
-    if n == 0 {
-        return (!records.contains(&Damaged)).into();
-    }
-    if n == 1 {
-        return one_damaged_group(records, nums[0] as usize);
-    }
-    let num_idx = n / 2;
-    let num = nums[num_idx] as usize;
-    // Find all possible places for `num`, then split the task in two smaller tasks.
-    records
-        .windows(num)
-        .positions(|window| !window.contains(&Operational))
-        .filter(|&pos| {
-            // Positions around must not be damaged or the window would be too large!
-            pos.checked_sub(1).map_or(true, |i| records[i] != Damaged)
-                && (pos + num < records.len())
-                    .then_some(pos + num)
-                    .map_or(true, |i| records[i] != Damaged)
-        })
-        .map(|pos| {
-            let c0 = (num_idx != 0).then(|| {
-                multiple_damaged_groups(&records[..pos.saturating_sub(1)], &nums[..num_idx])
-            });
-            let c1 = (c0 != Some(0)).then(|| {
-                multiple_damaged_groups(
-                    &records[(pos + num + 1).min(records.len())..],
-                    &nums[num_idx + 1..],
-                )
-            });
-            match (c0, c1) {
-                (None, None) => 0,
-                (Some(c), None) | (None, Some(c)) => c,
-                (Some(c0), Some(c1)) => c0 * c1,
-            }
-        })
-        .sum()
+    let total = if n == 0 {
+        (!records.contains(&Damaged)).into()
+    } else if n == 1 {
+        one_damaged_group(records, nums[0] as usize)
+    } else {
+        let num_idx = n / 2;
+        let num = nums[num_idx] as usize;
+        // Find all possible places for `num`, then split the task in two smaller tasks.
+        records
+            .windows(num)
+            .positions(|window| !window.contains(&Operational))
+            .filter(|&pos| {
+                // Positions around must not be damaged or the window would be too large!
+                pos.checked_sub(1).map_or(true, |i| records[i] != Damaged)
+                    && (pos + num < records.len())
+                        .then_some(pos + num)
+                        .map_or(true, |i| records[i] != Damaged)
+            })
+            .map(|pos| {
+                let c0 = (num_idx != 0).then(|| {
+                    multiple_damaged_groups(
+                        cache,
+                        &records[..pos.saturating_sub(1)],
+                        &nums[..num_idx],
+                    )
+                });
+                let c1 = (c0 != Some(0)).then(|| {
+                    multiple_damaged_groups(
+                        cache,
+                        &records[(pos + num + 1).min(records.len())..],
+                        &nums[num_idx + 1..],
+                    )
+                });
+                match (c0, c1) {
+                    (None, None) => 0,
+                    (Some(c), None) | (None, Some(c)) => c,
+                    (Some(c0), Some(c1)) => c0 * c1,
+                }
+            })
+            .sum()
+    };
+    cache.insert(key, total);
+    total
+}
+
+/// It depends on limits I assume from the inputs.
+type CacheKey = ([u64; 4], u128);
+
+fn cache_key(records: &[Record], nums: &[u8]) -> CacheKey {
+    // Each line has 20 records max, so 104 for part 2. So 208 bits is enough to know `records`.
+    // [u64; 4] (256 bits) is therefore enough.
+    let r = records.len();
+    let rec_id = core::array::from_fn(|i| {
+        // Slices does not link out-of-bound ranges.
+        records[r.min(32 * i)..r.min(32 * (i + 1))]
+            .iter()
+            .fold(0, |acc, rec| {
+                acc << 2
+                    | match rec {
+                        Operational => 0b01,
+                        Damaged => 0b10,
+                        Unknown => 0b11,
+                    }
+            })
+    });
+    // Each line has 6 nums max, so 30 for part 2. The max number is 15 (4 bits).
+    // So 120 bits is enough to know `nums`.
+    let n_id = nums.iter().fold(0u128, |acc, n| acc << 4 | u128::from(*n));
+    (rec_id, n_id)
 }
 
 pub const INPUTS: [&str; 2] = [
@@ -148,7 +193,8 @@ fn solver_23_12() -> Result<()> {
 
 #[test]
 fn examples() {
-    let part2_from = |s: &str, nums: &[u8]| {
+    let mut cache = HashMap::new();
+    let mut part2_from = |s: &str, nums: &[u8]| {
         let records = [s; 5]
             .join("?")
             .chars()
@@ -158,7 +204,7 @@ fn examples() {
             .flatten()
             .copied()
             .collect_vec();
-        multiple_damaged_groups(&records, &nums)
+        multiple_damaged_groups(&mut cache, &records, &nums)
     };
     assert_eq!(part2_from("???.###", &[1, 1, 3]), 1);
     assert_eq!(part2_from(".??..??...?##.", &[1, 1, 3]), 16384);
