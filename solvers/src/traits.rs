fd4ffad25ad7abeff6990ab80190a6d303e92aaf
Philippe-Cholet <phcholet@orange.fr>
2023-12-28 17:43:36 +0100

    Limit answer types a solver can return
    Otherwise, I could cheat myself by moving all the work to the implementation of `Display` of some arbitrary struct.
    But it won't be the only usage or I would not care that much.

--- solvers/src/traits.rs
+++ solvers/src/traits.rs
@@ -2,14 +2,31 @@ use std::time::{Duration, Instant};
 
 use common::{Part, Result};
 
+trait SolverAnswer: std::fmt::Display {}
+
 pub trait AocSolver {
     fn solve(&self, part: Part, input: &str) -> Result<String>;
     fn timed_solve(&self, part: Part, input: &str) -> Result<(String, Duration)>;
 }
 
+impl SolverAnswer for String {}
+impl SolverAnswer for &'static str {}
+impl SolverAnswer for u8 {}
+impl SolverAnswer for u16 {}
+impl SolverAnswer for u32 {}
+impl SolverAnswer for usize {}
+impl SolverAnswer for u64 {}
+impl SolverAnswer for u128 {}
+impl SolverAnswer for i8 {}
+impl SolverAnswer for i16 {}
+impl SolverAnswer for i32 {}
+impl SolverAnswer for isize {}
+impl SolverAnswer for i64 {}
+impl SolverAnswer for i128 {}
+
 impl<T, F> AocSolver for F
 where
-    T: std::fmt::Display,
+    T: SolverAnswer,
     F: Fn(Part, &str) -> Result<T>,
 {
     fn solve(&self, part: Part, input: &str) -> Result<String> {
